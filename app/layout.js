dbb388cdc43c88cf904b79e9f9d288e3dfbfb35d
Jorge Acosta <jorge.acosta-parra@mytheresa.com>
2022-11-12 09:20:56 +0100

    Init project

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  // children: React.ReactNode;
+  children,
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
